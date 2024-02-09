514b870eaed0222b169c11f096f86a5b10a17bd3
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2024-02-09 17:37:30 +0700

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('telp');
+            $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('token')->nullable();
+            $table->string('location');
             $table->rememberToken();
             $table->timestamps();
         });
